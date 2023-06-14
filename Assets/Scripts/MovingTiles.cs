90c01782120a37c17d792fea4b14b4ea2e4bc8fa
Akuukis <akuukis@kalvis.lv>
2023-06-14 19:39:36 +0300

    feat: randomized tiles

--- Assets/Scripts/MovingTiles.cs
+++ Assets/Scripts/MovingTiles.cs
@@ -1,27 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingTiles : MonoBehaviour
 {
-    public float scrollSpeed = 1f;
+    public float scrollSpeed = 10f;
     private float z;
-    private Transform tile1;
-    private Transform tile2;
+    private int tile1 = 0;
+    private int tile2 = 1;
+    private List<Transform> tiles = new List<Transform>();
     private float tileSize;
+    private float distance = 0;
 
     private void Start()
     {
-        tile1 = transform.GetChild(0);
-        tile2 = Instantiate(tile1, tile1.position, tile1.rotation, transform);
-        tileSize = tile1.GetComponent<Renderer>().bounds.size.z;
-        z = tile1.position.z - tileSize;
+        foreach (Transform child in transform)
+        {
+            tiles.Add(child);
+            child.gameObject.SetActive(false);
+        }
+
+        tileSize = tiles[tile1].GetComponent<Renderer>().bounds.size.z;
+        z = tiles[tile1].position.z - tileSize;
     }
 
     private void Update()
     {
         float offset = Time.time * scrollSpeed;
+        if(offset > distance)
+        {
+            distance = distance + tileSize;
+            if(Mathf.Repeat(distance, tileSize * 2) == 0)
+            {
+                tiles[tile2].gameObject.SetActive(false);
+                tile2 = (int)(Random.value * (transform.childCount - 1));
+                if(tile2 == tile1) tile2 = (tile2 + 1) % transform.childCount; // Always use different tiles.
+                tiles[tile2].gameObject.SetActive(true);
+            } else {
+                tiles[tile1].gameObject.SetActive(false);
+                tile1 = (int)(Random.value * (transform.childCount - 1));
+                if(tile1 == tile2) tile1 = (tile1 + 1) % transform.childCount; // Always use different tiles.
+                tiles[tile1].gameObject.SetActive(true);
+            }
+        }
         float newPosition1 = Mathf.Repeat(offset, tileSize * 2);
         float newPosition2 = Mathf.Repeat(offset + tileSize, tileSize * 2);
-        tile2.localPosition = new Vector3(0, 0, z + tileSize * 2 - newPosition2);
-        tile1.localPosition = new Vector3(0, 0, z + tileSize * 2 - newPosition1);
+        tiles[tile1].localPosition = new Vector3(0, 0, z + tileSize * 2 - newPosition1);
+        tiles[tile2].localPosition = new Vector3(0, 0, z + tileSize * 2 - newPosition2);
     }
 }
